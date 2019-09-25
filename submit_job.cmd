87c12f58481a558c77f8b982b7a5f68c0679d927
javier.bejar <bejar@cs.upc.edu>
2019-09-25 10:53:32 +0200

    Some updating

--- submit_job.cmd
+++ submit_job.cmd
@@ -1,12 +1,14 @@
-#SBATCH --job-name="jobname"
-#SBATCH -D/gpfs/projects/group00/user00000/scriptdir
-#SBATCH --output=/gpfs/projects/group00/user00000/scriptdir/output.out
-#SBATCH --error=/gpfs/projects/group00/user00000/scriptdir/errors.err
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=1:50:00
-#SBATCH --gres=gpu:1
-#SBATCH --mem=8000
+#!/bin/bash
+# @ job_name = textjob
+# @ initialdir = /gpfs/projects/bsc28/bsc28642/Wind/text
+# @ output = job_text%j.out
+# @ error = job_text%j.err
+# @ total_tasks = 1
+# @ gpus_per_node = 1
+# @ cpus_per_task = 1
+# @ features = k80
+# @ wall_clock_limit = 47:50:00
+
 module purge
 module load K80 impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
 
