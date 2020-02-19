1f5f10e66a902a73bf17f36888331826957c470e
javier.bejar <bejar@cs.upc.edu>
2020-02-19 10:32:00 +0100

    Some updating

--- TextGeneration/textjob.cmd
+++ TextGeneration/textjob.cmd
@@ -1,15 +1,17 @@
 #!/bin/bash
-# @ job_name = textjob
-# @ initialdir = /gpfs/projects/bsc28/bsc28642/Wind/text
-# @ output = job_text%j.out
-# @ error = job_text%j.err
-# @ total_tasks = 1
-# @ gpus_per_node = 1
-# @ cpus_per_task = 1
-# @ features = k80
-# @ wall_clock_limit = 47:50:00
+#SBATCH --job-name=TextGeneration
+#SBATCH -D/gpfs/projects/bsc28/bsc28642/DL/DLMAI/TextGeneration
+#SBATCH --output=job_text_%j.out
+#SBATCH --error=job_text_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=00:05:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000
 
 module purge
-module load K80 impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
+module load  gcc/6.4.0  cuda/9.1 cudnn/7.1.3 openmpi/3.0.0 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.7 szip/2.1.1 ffmpeg/4.0.2 opencv/3.4.1 python/3.6.5_ML
+PYTHONPATH={/gpfs/projects/bsc28/bsc28642/DL/DLMAI/TextGeneration}
+export PYTHONPATH
 
-python TextGenerator.py --verbose --progress --save --iterations 15 --neurons 512 --layers 2 --exlen 100 --step 2 --datafile 4
+python TextGenerator.py --verbose --progress --save --iterations 1 --neurons 16 --layers 1 --exlen 10 --step 1 --datafile 1
--- submit_job.cmd
+++ submit_job.cmd
@@ -1,15 +1,17 @@
 #!/bin/bash
-# @ job_name = textjob
-# @ initialdir = /gpfs/projects/bsc28/bsc28642/Wind/text
-# @ output = job_text%j.out
-# @ error = job_text%j.err
-# @ total_tasks = 1
-# @ gpus_per_node = 1
-# @ cpus_per_task = 1
-# @ features = k80
-# @ wall_clock_limit = 47:50:00
+#SBATCH --job-name=NAME_OF_JOB
+#SBATCH -D/path/to/the/code
+#SBATCH --output=output_%j.out
+#SBATCH --error=errors_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --time=HH:MM:SS
+#SBATCH --gres=gpu:1
+#SBATCH --mem=MEMORY
 
 module purge
-module load K80 impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
+module load  gcc/6.4.0  cuda/9.1 cudnn/7.1.3 openmpi/3.0.0 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.7 szip/2.1.1 ffmpeg/4.0.2 opencv/3.4.1 python/3.6.5_ML
+PYTHONPATH={jobs_code_path}
+export PYTHONPATH
 
 python script.py --flag1 --flag2 > std_output.txt
